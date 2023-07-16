384ff53f70b47b9bb6de4e9a6081194d56b62d08
Jack Scorpio <zjztom@gmail.com>
2023-07-16 11:52:31 +1000

    Update config

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
-  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+	title: `Liberty Super | A Superannuation Administration Services Provider`,
+	description: `Liberty Super provides superannuation administration services to superannuation trustees and administrators and also takes care of administrative tasks for advisors.`,
+	url: `https://libertysuper.com`, // No trailing slash!
+	name: `Liberty Super`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
